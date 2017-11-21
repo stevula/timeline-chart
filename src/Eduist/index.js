06e753ca7c3a606e9ea1e13a05b6b4e1d55a2713
stevula <brodericksteven@gmail.com>
2017-11-20 18:12:54 -0800

    Add eduist info to Eduist component

--- src/Eduist/index.js
+++ src/Eduist/index.js
@@ -3,12 +3,17 @@ import React, { Component } from 'react';
 class Eduist extends Component {
   render() {
     return (
-      <rect
-        fill="steelblue"
-        height={this.props.height}
-        stroke="steelblue"
-        width={this.props.width}
-        x={this.props.x} />
+      <g transform={`translate(${this.props.x}, 0)`}>
+        <rect
+          fill="steelblue"
+          height={this.props.height}
+          stroke="steelblue"
+          width={this.props.width} />
+
+        <text y="20" fill="white" fontSize="10">
+          {`${this.props.name}: ${this.props.dob} — ${this.props.dod} `}
+        </text>
+      </g>
     );
   }
 }
--- src/Row/index.js
+++ src/Row/index.js
@@ -13,10 +13,14 @@ class Row extends Component {
   renderEduists() {
     return this.props.eduists.map((eduist) => {
       const startPoint = (eduist.birth - this.props.min) * this.props.scale;
-      // const width = (eduist.death - eduist.birth) * this.props.scale;
-      const width = '500'
+      const endPoint = eduist.death === -1 ? (new Date).getFullYear() : eduist.death;
+      const width = (endPoint - startPoint) * this.props.scale;
       return <Eduist
+        dob={eduist.birth}
+        dod={eduist.death}
         height={this.props.height}
+        name={eduist.name}
+        summary={eduist.summary}
         width={width}
         x={startPoint} />;
     });
