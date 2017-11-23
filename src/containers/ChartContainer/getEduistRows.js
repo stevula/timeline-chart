1898f81ecf278cc6ae6857b350088620fe5b5b68
stevula <brodericksteven@gmail.com>
2017-11-22 21:17:24 -0800

    Sort data by birth year

--- src/containers/ChartContainer/getEduistRows.js
+++ src/containers/ChartContainer/getEduistRows.js
@@ -1,17 +1,19 @@
 // return array of row arrays containing eduists
 const getEduistRows = (eduists) => {
-  return eduists.reduce((rows, eduist) => {
-    let availableRow = rows.find((row) => {
-      const latestRowDeath = row[row.length - 1].death;
-      return latestRowDeath !== -1 && latestRowDeath < eduist.birth;
-    });
-    if (!availableRow) {
-      availableRow = [];
-      rows.push(availableRow);
-    }
-    availableRow.push(eduist);
-    return rows;
-  }, []);
+  return eduists
+    .sort((a, b) => a.birth - b.birth)
+    .reduce((rows, eduist) => {
+      let availableRow = rows.find((row) => {
+        const latestRowDeath = row[row.length - 1].death;
+        return latestRowDeath !== -1 && latestRowDeath < eduist.birth;
+      });
+      if (!availableRow) {
+        availableRow = [];
+        rows.push(availableRow);
+      }
+      availableRow.push(eduist);
+      return rows;
+    }, []);
 };
 
 export default getEduistRows;
